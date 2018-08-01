8791ba84f3737dd13063d4f3d8e851a45cb0b43a
upputuriyamini <41769865+upputuriyamini@users.noreply.github.com>
2018-08-01 16:37:46 +0530

    Create al.py

---
+++ al.py
@@ -0,0 +1,8 @@
+al = a
+if al == 0:
+       exit();
+else:
+     if((al>='a' and al>='z') or (al>='A' and al>='Z')):
+     print(al, "is an alphabet.")
+     else:
+     print(al,"is not an alphabet")
