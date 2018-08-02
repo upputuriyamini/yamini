0af771db8ab9ae59fcb839ad096a4b944f201542
upputuriyamini <41769865+upputuriyamini@users.noreply.github.com>
2018-08-02 16:05:03 +0530

    Update ln.py

--- ln.py
+++ ln.py
@@ -3,8 +3,8 @@ b = int(input())
 c = int(input())
 if ((a>b) and (a>c)):
     largest = a
-    elif ((b>a) and (b>c)):
-          largest = b
-    else:
-          largest = c
+elif ((b>a) and (b>c)):
+     largest = b
+ else:
+      largest = c
 print(largest)
