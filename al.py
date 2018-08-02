9364839d75f0a052a886dfd96435e4084d2173db
upputuriyamini <41769865+upputuriyamini@users.noreply.github.com>
2018-08-02 15:09:07 +0530

    Update al.py

--- al.py
+++ al.py
@@ -1,8 +1,8 @@
-al = a
+al = int(rawinput())
 if al == 0:
        exit();
 else:
      if((al>='a' and al>='z') or (al>='A' and al>='Z')):
-     print(al, "is an alphabet.")
+                 print(al, "is an alphabet.")
      else:
-     print(al,"is not an alphabet")
+                 print(al,"is not an alphabet")
