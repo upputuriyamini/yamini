364cc34891805970807d85fffaa1cee12dae6452
upputuriyamini <41769865+upputuriyamini@users.noreply.github.com>
2018-08-02 15:54:46 +0530

    Update ln.py

--- ln.py
+++ ln.py
@@ -1,9 +1,9 @@
 a = int(input())
 b = int(input())
 c = int(input())
-if (a>b) and (a>c):
+if ((a>b) and (a>c)):
     largest = a
-    elif (b>a) and (b>c):
+    elif ((b>a) and (b>c)):
           largest = b
     else:
           largest = c
