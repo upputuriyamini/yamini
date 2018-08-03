1cbba0c968870ff41e4a4052f135fc7fe39da3c0
upputuriyamini <41769865+upputuriyamini@users.noreply.github.com>
2018-08-03 14:46:55 +0530

    Create vc.py

---
+++ vc.py
@@ -0,0 +1,8 @@
+la=int(input())
+if la>='a' and la<='z':
+           if la in ('a', 'e', 'i', 'o', 'u'):
+                     print("vowel")
+           else:
+                     print("consonant")
+else:
+           print("invalid")
