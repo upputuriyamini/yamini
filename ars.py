2f296dd2c4b47f0c813332e84e64ecdade6f135e
upputuriyamini <41769865+upputuriyamini@users.noreply.github.com>
2018-08-09 12:54:05 +0530

    Create ars.py

---
+++ ars.py
@@ -0,0 +1,11 @@
+lower,upper=map(int,raw_input().split())
+for num in range(lower,upper):
+    temp=num
+    sum=0
+    while(temp>0):
+          digit=temp%10
+          sum=sum+digit**3
+          temp=temp/10
+         if(sum==num):
+              print(num)
+               
