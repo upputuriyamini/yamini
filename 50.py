66e11ac12e6cefd1160f0e181852ed4c28e4c1e6
upputuriyamini <41769865+upputuriyamini@users.noreply.github.com>
2018-08-13 12:45:02 +0530

    Update 50.py

--- 50.py
+++ 50.py
@@ -1,6 +1,7 @@
+def isPowerOfTwo (x):
+  return (x and (not(x & (x - 1))) )
 n=int(input())
-for k in range (1,100):
-	if(n==2**k):
-                      	print("yes")
-            else:
-            	print("no")
+if(isPowerOfTwo(n)):
+    print("Yes")
+else:
+    print("No")
