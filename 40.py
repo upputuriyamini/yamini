ac0aa2713354de057cb3122224a3b15fccf2765f
upputuriyamini <41769865+upputuriyamini@users.noreply.github.com>
2018-08-13 15:45:59 +0530

    Create 40.py

---
+++ 40.py
@@ -0,0 +1,17 @@
+r=int(raw_input())
+n1=1
+n2=1
+i=2
+if r<= 0:
+       print("positive number")
+elif r== 1:
+       print n1
+else:
+       print n1
+       print n2
+       while i<r:
+              n3=n1+n2
+              print n3
+              n1=n2
+              n2=n3
+              i=i+1
