d3365c55c7688c7dd2e3753283ed15ba53f93359
upputuriyamini <41769865+upputuriyamini@users.noreply.github.com>
2018-08-30 12:41:15 +0530

    Create 56.py

---
+++ 56.py
@@ -0,0 +1,6 @@
+a=raw_input()
+b=list(a)
+if ("1" in b)or("2" in b)or("3" in b)or("4" in b)or("5" in b)or("6" in b)or("7" in b)or("8" in b)or("9" in b):
+                                       print ("Yes")
+else:
+                                       print ("No")
