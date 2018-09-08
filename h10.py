e53b237c60f0990b92949bc3249e34de6c73f872
upputuriyamini <41769865+upputuriyamini@users.noreply.github.com>
2018-09-08 12:00:20 +0530

    Update h10.py

--- h10.py
+++ h10.py
@@ -2,6 +2,6 @@ N,M=map(int,raw_input().split())
 A=[int(x) for x in raw_input().split()]
 B=[int(y) for y in raw_input().split()]
 if set(B).issubset(A):
-                  print('yes')
+                  print('YES')
 else:
-                  print('no')
+                  print('NO')
