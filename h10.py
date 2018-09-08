3cc9abaf9102fc7f3c87aff7ac0b9327fcc22d25
upputuriyamini <41769865+upputuriyamini@users.noreply.github.com>
2018-09-08 11:58:14 +0530

    Create h10.py

---
+++ h10.py
@@ -0,0 +1,7 @@
+N,M=map(int,raw_input().split())
+A=[int(x) for x in raw_input().split()]
+B=[int(y) for y in raw_input().split()]
+if set(B).issubset(A):
+                  print('yes')
+else:
+                  print('no')
