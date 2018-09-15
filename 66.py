6a1386ecda33e014cd3389fb7b9cd6af3666f073
upputuriyamini <41769865+upputuriyamini@users.noreply.github.com>
2018-09-15 16:31:36 +0530

    Create 66.py

---
+++ 66.py
@@ -0,0 +1,8 @@
+num=raw_inpt()
+if num>1:
+       for i in range (2,num):
+             if num%i==0:
+                  print ('no')
+                  break
+       else:
+             print ('yes')
